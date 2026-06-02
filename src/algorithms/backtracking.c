9ae65f74e648d3a149729b93d07791322d9c37ce
Jorge-Carrillo8456 <cuentaformaljorgecch@gmail.com>
2026-06-02 16:55:20 -0600

    Add backtracking allocation function

---
+++ src/algorithms/backtracking.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "algorithms.h"
+int backtracking_allocation(MemoryManager* mm, Stack* history, int pid, int size) {
+    stack_push(history, pid);
+    int address = brute_force_worst_fit(mm, size);
+    if (address == -1) {
+        int failed_pid = stack_pop(history);
+        return -1;
+    }
+    return address;
+}
