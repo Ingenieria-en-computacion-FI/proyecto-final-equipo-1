4d2cf483d395ba4bf9d50109192d36503f9e2496
Jorge-Carrillo8456 <cuentaformaljorgecch@gmail.com>
2026-06-02 11:57:55 -0600

    Generate process data and save to CSV file

--- scripts/generate_process.py
+++ scripts/generate_process.py
@@ -1,14 +1,12 @@
 import random
 
-for i in range(10):
-
-    process = {
-
-        "pid": i,
-
-        "burst": random.randint(1, 20),
-
-        "memory": random.randint(10, 500)
-    }
-
-    print(process)
+# Abrimos (o creamos) un archivo txt/csv para guardar los datos generados
+with open("data/inputs/procesos.csv", "w") as file:
+    for i in range(10):
+        burst = random.randint(1, 20)
+        memory = random.randint(10, 500)
+        
+        # Guardamos en formato: pid,burst_time,memory
+        file.write(f"{i},{burst},{memory}\n")
+        
+print("Archivo de procesos generado exitosamente en data/inputs/procesos.csv")
